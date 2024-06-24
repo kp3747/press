3cbd0cfca6d9d62ab102af29b9f84ee2d584cb7a
cdmaczane <cdmaczane@hotmail.com>
2024-06-23 21:20:43 -0700

    Sketched out initial document structures.

--- src/press.h
+++ src/press.h
@@ -27,5 +27,83 @@ enum
 
 static_assert((page_size & (page_size - 1)) == 0); // Ensure power of two
 
+typedef enum
+{
+	document_element_type_heading_2,
+	document_element_type_heading_3,
+	document_element_type_paragraph,
+	document_element_type_blockquote,
+	document_element_type_preformatted,
+	document_element_type_unordered_list,
+	document_element_type_ordered_list_roman,
+	document_element_type_ordered_list_arabic
+} document_element_type;
+
+typedef enum
+{
+	ordered_list_type_arabic,
+	ordered_list_type_
+} ordered_list_type;
+
+typedef struct
+{
+	uint32_t day	: 5;
+	uint32_t month	: 4;
+	uint32_t year	: 23;
+} date;
+
+typedef struct
+{
+	const char*		title;
+	const char**	authors;
+	const char**	translators;
+	date			written;
+	date			published;
+	uint32_t		author_count;
+	uint32_t		translator_count;
+} document_metadata;
+
+typedef struct
+{
+	const char*	text;
+	uint32_t	level;
+} document_heading;
+
+typedef struct
+{
+	const char* text;
+} document_paragraph;
+
+typedef struct
+{
+	const char* text;
+} document_preformatted;
+
+typedef struct
+{
+	document_element_type	type;
+	const char*				text;
+} document_element;
+
+typedef struct
+{
+	const char*	text;
+} document_reference;
+
+typedef struct
+{
+	document_element*	elements;
+	document_reference*	references;
+	uint32_t			element_count;
+	uint32_t			reference_count;
+} document_chapter;
+
+typedef struct
+{
+	document_metadata	metadata;
+	document_chapter*	chapters;
+	uint32_t			chapter_count;
+} document;
+
 noreturn
 static void handle_parse_error(const char* label, uint32_t line, uint32_t column, const char* format, ...);
